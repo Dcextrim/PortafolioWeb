8c611514659f3aa0770237dcda4e677cdc81e652
Poposit <che231177@uvg.edu.gt>
2026-03-17 19:11:06 -0600

    UPDATE: CV

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -82,7 +82,7 @@ const HeroSection = () => {
               <div className="mt-8 md:ml-2 flex flex-col gap-3">
                 <Link
                   href={
-                    "https://drive.google.com/file/d/1aaIogxxmc_0zwYNykKoVrgkCXcKygfU4/view?usp=sharing"
+                    "https://drive.google.com/file/d/15vuBb0m7L5UNJtnXRKNfKUt_VBT4Jwom/view?usp=sharing"
                   }
                   target="_blank"
                   className="flex-1"
